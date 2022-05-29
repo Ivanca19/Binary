1a35529226dce305bafad34de41f3db07ca74d03
Ivanca19 <105860596+Ivanca19@users.noreply.github.com>
2022-05-29 18:08:27 -0400

    Update Binary.java

--- BinaryProject/src/game/Binary.java
+++ BinaryProject/src/game/Binary.java
@@ -1,5 +1,7 @@
 package game;
-
+/*This class creates a Binary object. A binary object uses two main arrays. An array of reference integers that will be represented by
+an 8 digit binary code, and a boolean array that will be used to represent if the binary code is made up of "1"s or "0"s
+*/
 public class Binary {
     private int[] _impasta = {128, 64, 32, 16, 8, 4, 2, 1};
 
