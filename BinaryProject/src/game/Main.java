9e1fb0731b078f767eef9183c362bc418aea648d
Ivanca19 <105860596+Ivanca19@users.noreply.github.com>
2022-05-29 18:24:47 -0400

    Update Main.java

--- BinaryProject/src/game/Main.java
+++ BinaryProject/src/game/Main.java
@@ -2,6 +2,7 @@ package game;
 
 import javax.swing.*;
 
+//My Main class creates my main method which simply initializes my Overseer object and runs the program untill the user closes the program
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         JOptionPane.showMessageDialog(null,"Welcome to my Binary Project.");
